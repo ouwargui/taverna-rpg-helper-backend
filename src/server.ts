933fc5b84244a5b46bcf2d6bc9d2aed1462aee71
ouwargui <guikemon2@gmail.com>
2022-09-10 20:19:35 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import {ErrorHandler} from './middlewares/ErrorHandler';
 import {routes} from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', routes);
+
+app.use(ErrorHandler);
 
 export {app};
